ab0ac26af1bafe8970ad7878dbe2e9e562682495
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2024-12-26 07:26:29 +0500

    Time: 2 ms (18.86%), Space: 8.7 MB (24.81%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+        int numJewelsInStones(string jewels, string stones)
+        {
+            unordered_set<char> us;
+            int count = 0;
+
+            for (const auto &jew: jewels)
+                us.insert(jew);
+
+            for (const auto &sto: stones)
+                if (us.find(sto) != us.end())
+                    count++;
+
+            return count;
+        }
+};
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+// map<char, int> count;
+// //counting all stones
+// for (int i = 0; i < stones.size(); i++)
+//     count[stones[i]]++;
+
+// //sum all valid jewels stone
+// for (int i = 0; i < jewels.size(); i++)
+//     ans += count[jewels[i]];
