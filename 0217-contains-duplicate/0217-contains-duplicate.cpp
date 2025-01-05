cf2f68e2791e2eda2becb6cd362c8adee38fba95
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-01-05 05:16:44 +0500

    Time: 47 ms (24.82%), Space: 75.8 MB (10.55%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen(nums.begin(), nums.end());
+        return seen.size() < nums.size();
+    }
+};
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+// unordered_set<int> seen;
+// for(auto& num : nums){
+//     if(seen.count(num))
+//         return true;
+//     seen.insert(num);
+// }
+// return false;
+
+// sort(nums.begin(), nums.end());
+// for(int i = 0; i < nums.size() - 1; i++)
+//     if(nums[i] == nums[i + 1]) return true;
+// return false;
+// Time Complexity: O(n log n)
+// Space complexity: O(1) (in-place sorting) or O(log n) (due to recursion in sorting)
