2e7af23f154a8367172aa09cd1e925980d98be22
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-01-06 07:34:51 +0500

    Time: 15 ms (86.62%), Space: 37.4 MB (37.13%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
-        vector<int> diff1, diff2;
-        
-        for (auto& num : set1) {
-            if (set2.find(num) == set2.end()) {
+        unordered_set<int> mp1(nums1.begin(), nums1.end());
+        unordered_set<int> mp2(nums2.begin(), nums2.end());
+        vector<int> diff1;
+        vector<int> diff2;
+
+        for(auto& num : mp1)
+            if(mp2.count(num) == 0)
                 diff1.push_back(num);
-            }
-        }
         
-        for (auto& num : set2) {
-            if (set1.find(num) == set1.end()) {
+        for(auto& num : mp2)
+            if(mp1.count(num) == 0) // if(mp1.find(num) == mp1.end())
                 diff2.push_back(num);
-            }
-        }
         
         return {diff1, diff2};
     }
 };
-// Time Complexity: O(n1 + n2)  
-// Space Complexity: O(n1 + n2)
+// Time Complexity: O(n)  
+// Space Complexity: O(n)
