692a3b959baf223734cf3ba7986f79ce339a4e56
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-01-10 15:08:04 +0500

    Time: 0 ms (100%), Space: 23.7 MB (75.41%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+        vector<vector < int>> merge(vector<vector < int>> &intervals)
+        {
+            sort(intervals.begin(), intervals.end());
+            vector<vector < int>> ans;
+            for (auto &in: intervals)
+                if (ans.empty() || ans.back()[1] < in[0])
+                    ans.push_back(in);
+                else
+                    ans.back()[1] = max(ans.back()[1], in[1]);
+
+            return ans;
+        }
+};
+// Time complexity: O(n logn)
+// Space complexity: O(n)
