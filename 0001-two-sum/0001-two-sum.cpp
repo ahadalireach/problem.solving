cee71dc171411dbb207e493d24953801bd505f9e
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-01-30 21:30:02 +0500

    Time: 0 ms (100%), Space: 14.6 MB (54.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,16 +2,35 @@ class Solution
 {
     public:
         vector<int> twoSum(vector<int> &nums, int target) {
-            unordered_map<int, int> mp;
-            for(int i = 0; i < nums.size(); i++){
-                int complement = target - nums[i];
-                if(mp.find(complement) != mp.end())
-                    return {mp[complement], i};
-                mp[nums[i]] = i;
+            vector<pair<int, int>> arr;  
+            for (int i = 0; i < nums.size(); i++)
+                arr.push_back({nums[i], i}); 
+            sort(arr.begin(), arr.end());
+
+            int l = 0, r = nums.size() - 1;
+            while (l < r) {
+                int sum = arr[l].first + arr[r].first;
+                if (sum == target) 
+                    return {arr[l].second, arr[r].second};
+                else if (sum > target) 
+                    r--;
+                else
+                    l++;
             }
-            return {};
+            return {}; 
         }
 };
+// Time complexity: O(n log n)
+// Space complexity: O(1)  (ignoring sorting for now, as we use arr, but no extra DS) 
+
+// unordered_map<int, int> mp;
+// int n = nums.size();
+// for(int i = 0; i < n; i++){
+//     int complement = target - nums[i];
+//     if(mp.find(complement) != mp.end())
+//         return {mp[complement], i};
+//     mp[nums[i]] = i;
+// }
 // Time complexity: O(n)
 // Space complexity: O(n)
 
