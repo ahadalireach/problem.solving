7b41168910c01ee7a6ce295309abfa477fd2be97
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-04-08 18:53:59 +0500

    Time: 0 ms (100%), Space: 18.4 MB (97.77%) - LeetHub

---
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                char smaller = min(s[left], s[right]);
+                s[left] = s[right] = smaller;
+            }
+            left++;
+            right--;
+        }
+        return s;
+    }
+};
