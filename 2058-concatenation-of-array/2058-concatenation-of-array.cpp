a1b0e6fc6067d128cbbc0d2db7f4780b4628cbaf
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-04-26 12:58:22 +0500

    Time: 0 ms (100%), Space: 16.9 MB (63.34%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i++){
-            nums.push_back(nums[i]);
-        }
-        
-        return nums;
+        vector<int> ans(nums.begin(), nums.end());
+        ans.insert(ans.end(), nums.begin(), nums.end());
+        return ans;
     }
 };
 
@@ -17,3 +13,11 @@ public:
 //     ans[i] = nums[i];
 //     ans[n + i] = nums[i];
 // }
+
+// 3rd Approach
+
+// int n = nums.size();
+// for(int i = 0; i < n; i++){
+//     nums.push_back(nums[i]);
+// }
+// return nums;
