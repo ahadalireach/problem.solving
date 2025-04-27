d898765a4726295bce2297eb193d7a589938bfa6
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-04-27 11:54:56 +0500

    Time: 0 ms (100%), Space: 9.8 MB (48.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+        for(int i = 0; i < s.length(); i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        
+        for(int c : count){
+            if(c != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+
+// Approach 2
+
+// unordered_map<char, int> freqMap;
+// for(char ss : s){
+//     freqMap[ss]++;
+// }
+// for(char tt : t){
+//     if(freqMap[tt] <= 0){
+//         return false;
+//     }
+//     freqMap[tt]--;
+// }
+// return true;
+
+
+// Approach 3 (Just Difference in Second Loop)
+
+// for(char tt : t){
+//     freqMap[tt]--;
+// }
+// for(auto it : freqMap){
+//     if(it.second != 0){
+//         return false;
+//     }
+// }
+
+// Time complexity: O(n + m)
+// Space complexity: O(1) (Only 26 letters are possible, so the map has at most 26 keys)
+
+// unordered_map automatically inserts it with a default value of 0 (important C++ behavior).
+
+
+// Approach 4
+
+// sort(s.begin(), s.end());
+// sort(t.begin(), t.end());
+// return s == t;
+
+// Time complexity: O(n logn + m logm)
+// Space complexity: O(1) or O(n + m) depending on the sorting algorithm.
