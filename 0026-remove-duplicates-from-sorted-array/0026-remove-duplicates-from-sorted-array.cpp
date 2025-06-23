6f3b1a3b90f461b4cfb6e13c454164ed7143170f
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-06-23 12:09:55 +0500

    Time: 0 ms (100%), Space: 22.5 MB (78.33%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -31,7 +31,7 @@ public:
 
 // int k = 0;
 // for(int i = 0; i < nums.size(); i++){
-//     if(nums[i] != nums[i + 1]){
+//     if (i == nums.size() - 1 || nums[i] != nums[i + 1]) {
 //         nums[k++] = nums[i];
 //     }
 // }
