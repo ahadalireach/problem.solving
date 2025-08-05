1745b60befaf3aca7d7f34a29b7bd1c772029979
Ahad Ali <115062995+ahadalireach@users.noreply.github.com>
2025-08-05 17:54:24 +0500

    Time: 0 ms (100%), Space: 24.6 MB (49.94%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int res = 0;
+
+        for(auto hour : hours)
+            if(hour >= target) res++;
+        
+        return res;
+    }
+};
